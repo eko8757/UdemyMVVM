574fd88e5d128bc2c11a0cfaea8053a57e3ccd8f
MasshookPakEko <ekomurdiansyah89@gmail.com>
2020-04-25 00:55:43 +0700

    add data binding

--- app/src/main/java/com/masshookpakeko/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/masshookpakeko/dogs/view/DetailFragment.kt
@@ -6,11 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 
 import com.masshookpakeko.dogs.R
+import com.masshookpakeko.dogs.databinding.FragmentDetailBinding
 import com.masshookpakeko.dogs.utils.getProgressDrawable
 import com.masshookpakeko.dogs.utils.loadImage
 import com.masshookpakeko.dogs.viewmodel.DetailViewModel
@@ -21,6 +23,8 @@ import kotlinx.android.synthetic.main.fragment_detail.*
  */
 class DetailFragment : Fragment() {
 
+    private lateinit var dataBinding: FragmentDetailBinding
+
     private lateinit var viewModel: DetailViewModel
     private var dogUuid = 0
 
@@ -29,7 +33,8 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -48,11 +53,7 @@ class DetailFragment : Fragment() {
     private fun observeViewModel() {
         viewModel.detailViewModel.observe(this, Observer { dog ->
             dog?.let {
-                tv_dog_name_detail.text = dog.name
-                tv_dog_purpose_detail.text = dog.bredFor
-                tv_dog_temperament_detail.text = dog.temperament
-                tv_dog_lifespan_detail.text = dog.lifeSpan
-                context?.let { img_dog_detail.loadImage(dog.url, getProgressDrawable(it)) }
+                dataBinding.dog = dog
             }
         })
     }
